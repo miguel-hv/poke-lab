c0e5baf0c5310d97757592041cb1b3ba3193dd02
miguel-hv <huertadelavegamiguel@gmail.com>
2024-05-26 10:14:09 +0200

    FEAT: modal oak

--- src/app/components/core/dialogs/dialog-info/dialog-info.component.ts
+++ src/app/components/core/dialogs/dialog-info/dialog-info.component.ts
@@ -1,46 +1,13 @@
-import { DIALOG_DATA, Dialog, DialogModule } from '@angular/cdk/dialog';
+import { DIALOG_DATA, DialogModule } from '@angular/cdk/dialog';
 import { Component, Inject } from '@angular/core';
-
-export interface DialogData {
-  animal: 'panda' | 'unicorn' | 'lion';
-}
-
-/**
- * @title Injecting data when opening a dialog
- */
 
 @Component({
   selector: 'app-dialog-info',
   standalone: true,
-  imports: [DialogModule],
   templateUrl: './dialog-info.component.html',
-  styleUrl: './dialog-info.component.scss'
+  styleUrl: './dialog-info.component.scss',
 })
 export class DialogInfoComponent {
-  constructor(public dialog: Dialog) {}
-
-  openDialog() {
-    this.dialog.open(CdkDialogDataExampleDialog, {
-      minWidth: '300px',
-      data: {
-        animal: 'panda',
-      },
-    });
-  }
-
+  constructor(@Inject(DIALOG_DATA) public data: {title: string, description: string}) {}
 }
 
-@Component({
-  selector: 'cdk-dialog-data-example-dialog',
-  templateUrl: 'cdk-dialog-data-example-dialog.html',
-  styleUrl: './cdk-dialog-data-example-dialog.scss',
-  standalone: true,
-})
-export class CdkDialogDataExampleDialog {
-  constructor(@Inject(DIALOG_DATA) public data: DialogData) {}
-}
-
-
-/**  Copyright 2024 Google LLC. All Rights Reserved.
-    Use of this source code is governed by an MIT-style license that
-    can be found in the LICENSE file at https://angular.io/license */
--- src/app/pages/select-pokemon-page/select-pokemon-page.component.ts
+++ src/app/pages/select-pokemon-page/select-pokemon-page.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DialogInfoComponent } from '../../components/core/dialogs/dialog-info/dialog-info.component';
+import { Dialog, DialogModule } from '@angular/cdk/dialog';
 
 @Component({
   selector: 'app-select-pokemon-page',
   standalone: true,
-  imports: [],
+  imports: [DialogModule],
   templateUrl: './select-pokemon-page.component.html',
   styleUrl: './select-pokemon-page.component.scss'
 })
 export class SelectPokemonPageComponent {
 
+  dialog = inject(Dialog);
+
+  openDialog() {
+    this.dialog.open(DialogInfoComponent, {
+      minWidth: '100vw',
+      data: {
+        title: 'título',
+        description: 'descripción'
+      },
+    });
+  }
 }
