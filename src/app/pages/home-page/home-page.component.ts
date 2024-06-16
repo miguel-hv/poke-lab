d1717d65b0083cae85216cda3683344ae6543cec
miguel-hv <huertadelavegamiguel@gmail.com>
2024-06-16 15:02:49 +0200

    FEAT: variable height in background image container

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, NgZone, OnDestroy, OnInit, ViewChild, inject } from '@angular/core';
 import { AuthService } from '../../shared/services/auth.service';
 import { RouterModule } from '@angular/router';
 
@@ -9,13 +9,34 @@ import { RouterModule } from '@angular/router';
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit, OnDestroy {
 
   public auth = inject(AuthService);
+  screenWidth = (screen.width * 1.777).toString()+"px";
+  widthObserver: ResizeObserver = {} as ResizeObserver;
 
-  constructor() { 
+  @ViewChild('containerWidth', { static: true }) containerWidth: ElementRef = {} as ElementRef;
+
+  constructor(
+    private zone: NgZone
+  ) { 
     if (this.auth.secrets().length === 3) 
       console.log("secretos conseguidos");
+  }
+
+  ngOnInit() {
+    this.widthObserver = new ResizeObserver(entries => {
+      this.zone.run(() => {
+        const screenWidth = entries[0].contentRect.width;
+        this.screenWidth = (screenWidth * 1.777).toString()+"px";
+      });
+    });
+
+    this.widthObserver.observe(this.containerWidth.nativeElement);
+  }
+
+  ngOnDestroy() {
+    this.widthObserver.unobserve(this.containerWidth.nativeElement);
   }
 
 
