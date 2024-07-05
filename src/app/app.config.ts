d9cf56456328903a70dc092fad4217d798d93a8e
miguel-hv <huertadelavegamiguel@gmail.com>
2024-07-05 23:34:43 +0200

    FEAT: investigate dialog position

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, inject } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { DEFAULT_DIALOG_CONFIG, DialogConfig } from '@angular/cdk/dialog';
+import { OverlayPositionBuilder } from '@angular/cdk/overlay';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()]
+
+  providers: [
+    provideRouter(routes), 
+    provideHttpClient(), 
+    {provide: DialogConfig}
+    // {provide: DEFAULT_DIALOG_CONFIG, useValue: {hasBackdrop: false}}
+    // {provide: DEFAULT_DIALOG_CONFIG, useValue: {positionStrategy: OverlayPositionBuilder.global().centerHorizontally()}},
+  ]
 };
--- src/app/components/core/dialogs/dialog-info/dialog-info.component.ts
+++ src/app/components/core/dialogs/dialog-info/dialog-info.component.ts
@@ -1,5 +1,7 @@
-import { DIALOG_DATA, DialogModule, DialogRef } from '@angular/cdk/dialog';
-import { Component, Inject } from '@angular/core';
+import { DEFAULT_DIALOG_CONFIG, DIALOG_DATA, DialogConfig, DialogRef } from '@angular/cdk/dialog';
+import { Overlay, OverlayPositionBuilder } from '@angular/cdk/overlay';
+import { CdkPortal } from '@angular/cdk/portal';
+import { Component, Inject, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-dialog-info',
@@ -12,10 +14,20 @@ export class DialogInfoComponent {
   private OK = 'OK';
   private NO = 'NO';
 
+    positionStrategyDef = this.overlayPosBuilder.global().bottom('0').centerHorizontally();
+
   constructor(
     @Inject(DIALOG_DATA) public data: {title: string, description: string, ok: string, no?: string},
-    public dialogRef: DialogRef<string>
-  ) {}
+    public dialogRef: DialogRef<string>,
+    private readonly overlayPosBuilder: OverlayPositionBuilder,
+    private config: DialogConfig,
+  ) {
+    //TODO: ¿como añadir positionStrategyDef que está arriba?
+    this.config.positionStrategy?.attach(this.dialogRef.overlayRef);
+  }
+
+
+
 
   accept() {
     this.dialogRef.close(this.OK);
